57c4a55f19724900f30d84183d76aef2ba86ca11
Scott T Gould <greysilly7@gmail.com>
2023-04-21 21:06:17 -0400

    Get rid of warnings and reduce intents

--- Program.cs
+++ Program.cs
@@ -4,52 +4,52 @@ using Discord.WebSocket;
 
 namespace POCBotButCSharp
 {
-  public class Program
-  {
-    private DiscordSocketClient? _client;
-    private InteractionHandler? _interactionHandler;
-    private readonly DiscordSocketConfig _socketConfig = new()
-    {
-      GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
-      AlwaysDownloadUsers = true,
-    };
+	public class Program
+	{
+		private DiscordSocketClient? _client;
+		private InteractionHandler? _interactionHandler;
+		private readonly DiscordSocketConfig _socketConfig = new()
+		{
+			GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers,
+			AlwaysDownloadUsers = true,
+		};
 
-    public static Task Main(string[] args) => new Program().MainAsync(args);
-    public async Task MainAsync(string[] args)
-    {
-      // ...
-      DotNetEnv.Env.Load();
+		public static Task Main(string[] args) => new Program().MainAsync(args);
+		public async Task MainAsync(string[] args)
+		{
+			// ...
+			DotNetEnv.Env.Load();
 
-      _client = new DiscordSocketClient(_socketConfig);
-      _client.Log += Log;
+			_client = new DiscordSocketClient(_socketConfig);
+			_client.Log += Log;
 
-      var _interactionService = new InteractionService(_client.Rest);
-      _interactionHandler = new InteractionHandler(_client, _interactionService);
-      await _interactionHandler.InitializeAsync();
+			var _interactionService = new InteractionService(_client.Rest);
+			_interactionHandler = new InteractionHandler(_client, _interactionService);
+			await _interactionHandler.InitializeAsync();
 
-      await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("TOKEN"));
-      await _client.StartAsync();
-      await Task.Delay(-1);
-    }
+			await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("TOKEN"));
+			await _client.StartAsync();
+			await Task.Delay(-1);
+		}
 
-    private Task Log(LogMessage msg)
-    {
-      Console.WriteLine(msg.ToString());
-      return Task.CompletedTask;
-    }
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
 
-    public static bool IsDebug()
-    {
-      bool debug = false;
+		public static bool IsDebug()
+		{
+			bool debug = false;
 
-      // Check if a command-line argument named "debug" is present
-      string[] args = Environment.GetCommandLineArgs();
-      if (args.Contains("debug"))
-      {
-        debug = true;
-      }
+			// Check if a command-line argument named "debug" is present
+			string[] args = Environment.GetCommandLineArgs();
+			if (args.Contains("debug"))
+			{
+				debug = true;
+			}
 
-      return debug;
-    }
-  }
+			return debug;
+		}
+	}
 }
--- commands/InfoModule.cs
+++ commands/InfoModule.cs
@@ -4,82 +4,82 @@ using Newtonsoft.Json;
 
 namespace POCBotButCSharp
 {
-  public class InfoModule : InteractionModuleBase<SocketInteractionContext>
-  {
-    [SlashCommand("ping", "Pong!")]
-    public async Task PingAsync()
-    {
-      // Send a response containing Latency
-      await RespondAsync($"GNIP! Latency: {Context.Client.Latency}ms");
-    }
+	public class InfoModule : InteractionModuleBase<SocketInteractionContext>
+	{
+		[SlashCommand("ping", "Pong!")]
+		public async Task PingAsync()
+		{
+			// Send a response containing Latency
+			await RespondAsync($"GNIP! Latency: {Context.Client.Latency}ms");
+		}
 
-    [SlashCommand("checkserver", "Checks if the Minecraft server is alive.")]
-    public async Task CheckServerAsync([Choice("Project OpenComputers 3", "poc3.namelessserver.net"), Choice("Warpy", "warpy.namelessserver.net")] string server)
-    {
+		[SlashCommand("checkserver", "Checks if the Minecraft server is alive.")]
+		public async Task CheckServerAsync([Choice("Project OpenComputers 3", "poc3.namelessserver.net"), Choice("Warpy", "warpy.namelessserver.net")] string server)
+		{
+			await DeferAsync();
+			using (var client = new HttpClient())
+			{
+				var response = await client.GetAsync($"https://api.mcsrvstat.us/2/{server}");
+				var content = await response.Content.ReadAsStringAsync();
+				var status = JsonConvert.DeserializeObject<QuickType.ServerData>(content);
+				// Rest of the code
+				if (status != null && status.Players != null && status.Motd != null && status.Motd.Raw != null && status.Online)
+				{
+					var serverInfoEmbed = new Discord.EmbedBuilder()
+					  .WithTitle($"Server Info for {server}: {(status.Online ? "✅" : "❌")}")
+					  .AddField("Total Players: ", status.Players.Online > 0 ? $"{status.Players.Online}/{status.Players.Max}" : "None", true)
+					  .WithDescription($"```{string.Join(" ", status.Motd.Raw)}```")
+					  .WithColor(Discord.Color.Blue)
+					  .Build();
+					await FollowupAsync(embed: serverInfoEmbed);
+				}
+			}
+		}
 
-      using (var client = new HttpClient())
-      {
-        var response = await client.GetAsync($"https://api.mcsrvstat.us/2/{server}");
-        var content = await response.Content.ReadAsStringAsync();
-        var status = JsonConvert.DeserializeObject<QuickType.ServerData>(content);
-        // Rest of the code
-        if (status != null && status.Players != null && status.Online)
-        {
-          var serverInfoEmbed = new Discord.EmbedBuilder()
-            .WithTitle($"Server Info for {server}: {(status.Online ? "✅" : "❌")}")
-            .AddField("Total Players: ", status.Players.Online > 0 ? $"{status.Players.Online}/{status.Players.Max}" : "None", true)
-            .WithDescription($"```{string.Join(" ", status.Motd.Raw)}```")
-            .WithColor(Discord.Color.Blue)
-            .Build();
-          await RespondAsync(embed: serverInfoEmbed);
-        }
-      }
-    }
 
+		/*
+		I don't really get this bit it does a news command with two subcommands: subscribe and unsubscribe 
+		*/
+		[Discord.Interactions.Group("news", "Lets you subscribe and unsubscribe to news")]
+		public class NewsGroup : InteractionModuleBase<SocketInteractionContext>
+		{
+			public enum NewsRoles : ulong
+			{
+				[ChoiceDisplay("Server updates (News Letter)")]
+				ServerUpdates = 805078371725869066,
+				[ChoiceDisplay("Technical updates (Devlog Subscriber)")]
+				TechnicalUpdates = 944371601560969326
 
-    /*
-    I don't really get this bit it does a news command with two subcommands: subscribe and unsubscribe 
-    */
-    [Discord.Interactions.Group("news", "Lets you subscribe and unsubscribe to news")]
-    public class NewsGroup : InteractionModuleBase<SocketInteractionContext>
-    {
-      public enum NewsRoles : ulong
-      {
-        [ChoiceDisplay("Server updates (News Letter)")]
-        ServerUpdates = 805078371725869066,
-        [ChoiceDisplay("Technical updates (Devlog Subscriber)")]
-        TechnicalUpdates = 944371601560969326
+			}
 
-      }
+			[SlashCommand("subscribe", "Subscribe to news")]
+			public async Task SubscribeAsync([Choice("Server updates (News Letter)", "Technical updates (Devlog Subscriber)")] NewsRoles role)
+			{
+				if (Context.User is not SocketGuildUser user || Context.Guild is not SocketGuild guild)
+				{
+					await ReplyAsync("You must be in a guild to use this command.");
+					return;
+				}
 
-      [SlashCommand("subscribe", "Subscribe to news")]
-      public async Task SubscribeAsync([Choice("Server updates (News Letter)", "Technical updates (Devlog Subscriber)")] NewsRoles role)
-      {
-        if (Context.User is not SocketGuildUser user || Context.Guild is not SocketGuild guild)
-        {
-          await ReplyAsync("You must be in a guild to use this command.");
-          return;
-        }
+				var roleToAdd = guild.GetRole((ulong)role);
+				await user.AddRoleAsync(roleToAdd);
+				await RespondAsync($"Added role {roleToAdd.Name} to {user.Username}");
+			}
 
-        var roleToAdd = guild.GetRole((ulong)role);
-        await user.AddRoleAsync(roleToAdd);
-        await RespondAsync($"Added role {roleToAdd.Name} to {user.Username}");
-      }
+			[SlashCommand("unsubscribe", "Unsubscribe from news")]
+			public async Task UnsubscribeAsync([Choice("Server updates (News Letter)", "Technical updates (Devlog Subscriber)")] NewsRoles role)
+			{
+				if (Context.User is not SocketGuildUser user || Context.Guild is not SocketGuild guild)
+				{
+					await ReplyAsync("You must be in a guild to use this command.");
+					return;
+				}
 
-      [SlashCommand("unsubscribe", "Unsubscribe from news")]
-      public async Task UnsubscribeAsync([Choice("Server updates (News Letter)", "Technical updates (Devlog Subscriber)")] NewsRoles role)
-      {
-        if (Context.User is not SocketGuildUser user || Context.Guild is not SocketGuild guild)
-        {
-          await ReplyAsync("You must be in a guild to use this command.");
-          return;
-        }
+				var roleToRemove = guild.GetRole((ulong)role);
+				await user.RemoveRoleAsync(roleToRemove);
+				await RespondAsync($"Removed role {roleToRemove.Name} from {user.Username}");
+			}
 
-        var roleToRemove = guild.GetRole((ulong)role);
-        await user.RemoveRoleAsync(roleToRemove);
-        await RespondAsync($"Removed role {roleToRemove.Name} from {user.Username}");
-      }
-
-    }
-  }
+		}
+	}
 }
