fc61114dc382662a322e97e0c71a011f72594145
Scott Gould <greysilly7@gmail.com>
2024-10-05 00:14:35 -0400

    Refactor flake.nix to include flake-utils as an input
    Signed-off-by: Scott Gould <greysilly7@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,10 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     cargo2nix.url = "github:cargo2nix/cargo2nix/main";
+    flake-utils.follows = "cargo2nix/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      # inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
@@ -16,35 +17,33 @@
     nixpkgs,
     cargo2nix,
     rust-overlay,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [cargo2nix.overlays.default rust-overlay.overlays.default];
-    };
-    rustPkgs = pkgs.rustBuilder.makePackageSet {
-      packageFun = import ./Cargo.nix;
-      rustVersion = "latest";
-    };
-    # The workspace defines a development shell with all of the dependencies
-    # and environment settings necessary for a regular `cargo build`.
-    # Passes through all arguments to pkgs.mkShell for adding supplemental
-    # dependencies.
-    workspaceShell = rustPkgs.workspaceShell {
-      packages = [pkgs.rustfmt];
-      # shellHook = ''
-      #   export PS1="\033[0;31m☠dev-shell☠ $ \033[0m"
-      # '';
-    }; # supports override & overrideAttrs
-  in rec {
-    packages = {
-      pocbot = rustPkgs.workspace.pocbot."1.0.2" {};
-      default = packages.pocbot;
-
-      shell = devShells.default;
-    };
-    devShells."${system}" = {
-      default = workspaceShell;
-    };
-  };
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [cargo2nix.overlays.default];
+      };
+      rustPkgs = pkgs.rustBuilder.makePackageSet {
+        packageFun = import ./Cargo.nix;
+        rustVersion = "latest";
+      };
+      # The workspace defines a development shell with all of the dependencies
+      # and environment settings necessary for a regular `cargo build`.
+      # Passes through all arguments to pkgs.mkShell for adding supplemental
+      # dependencies.
+      workspaceShell = pkgs.mkShell {
+        # shellHook = ''
+        #   export PS1="\033[0;31m☠dev-shell☠ $ \033[0m"
+        # '';
+      };
+    in rec {
+      packages = {
+        pocbot = rustPkgs.workspace.pocbot {};
+        default = packages.pocbot;
+      };
+      devShells = {
+        default = workspaceShell;
+      };
+    });
 }
